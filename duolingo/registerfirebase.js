0d4ff722769bf8e9d26277a19927b4ca7e939145
Jirapat-4cus <s40597@bj.ac.th>
2025-07-24 22:17:22 +0700

    กูทำอะไรอยู่นะ

--- duolingo/registerfirebase.js
+++ duolingo/registerfirebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js';
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBU0x1OfkVMlKUTvu6zsgBo5V2m2tHkgJM",
@@ -12,29 +12,31 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
+window.addEventListener('DOMContentLoaded', () => {
+  const submit = document.getElementById('submit');
+  submit.addEventListener('click', function(event) {
+    event.preventDefault();
+    
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
 
+    if (password !== confirmPassword) {
+      alert("Passwords do not match!");
+      return;
+    }
 
-const submit = document.getElementById('submit');
-submit.addEventListener('click', function(event) {
-  event.preventDefault();
-
-  //input
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-
-  createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed up 
-    const user = userCredential.user;
-    alert('Registration successful! Welcome, ' + user.email);
-    window.location.href = "login.html";
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    alert('Error: ' + errorMessage);
-    // ..
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        alert('Registration successful! Welcome, ' + userCredential.user.email);
+        setTimeout(() => {
+          window.location.href = "login.html";
+        }, 500);
+      })
+      .catch((error) => {
+        alert('Error: ' + error.message);
+      });
   });
-})
+});
